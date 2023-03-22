dc5c615a6e2387b685bdb918c79f2ab43797b9f6
foxydevloper <12120644+foxydevloper@users.noreply.github.com>
2023-03-22 07:41:24 -0400

    add comment as to what omegga_io.ts does

--- omegga_io.ts
+++ omegga_io.ts
@@ -1,3 +1,8 @@
+// this is needed for deno_json_rpc to communicate with omegga,
+// which talks to the plugin via stdin/stdout
+// you can just ignore this script
+// based off https://github.com/n1c00o/deno-json-rpc/blob/main/src/vscode_io.ts
+// but with some changes to make it work with omegga
 import { IO } from "https://deno.land/x/deno_json_rpc@v1.0.0/src/rpc_two.ts";
 import { readLines } from "https://deno.land/std@0.180.0/io/read_lines.ts";
 
